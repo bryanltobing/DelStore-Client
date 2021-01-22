c99767ed2e0c2f9bd17eeaa81dc0a4f566d68624
bryantobing12 <bryantobing0@gmail.com>
2021-01-22 10:50:39 +0700

    Fix: Facility image change

--- src/components/Facility/FacilityItemBody.js
+++ src/components/Facility/FacilityItemBody.js
@@ -12,7 +12,7 @@ const FacilityItemBody = ({ facilityInfo }) => {
             <div className="flex-shrink-0 h-10 w-10">
               <img
                 className="h-10 w-10 rounded-full"
-                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
+                src="https://img.icons8.com/ios/452/room.png"
                 alt="fac"
               />
             </div>
